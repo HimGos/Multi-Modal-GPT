9dac3f710a3204dd662e820c0c8122b0e41173ae
Himanshu Goswami <himgos@gmail.com>
2024-01-20 12:05:17 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
 import streamlit as st
 
+
+def clear_input_field():
+    st.session_state.user_question = st.session_state.user_input
+    st.session_state.user_input = ""
+
+
+def set_send_input():
+    st.session_state.send_input = True
+    clear_input_field()
+
+
 def main():
-    st.title("Multimodal Local Chat App")
+    st.title("Multimodal GPT")
     chat_container = st.container()
 
-    user_input = st.text_input("Type your message here", key='user_input')
+    if "send_input" not in st.session_state:
+        st.session_state.send_input = False
+        st.session_state.user_question = ""
+
+    user_input = st.text_input("Type your message here", key='user_input', on_change=set_send_input)
 
     send_button = st.button("Send", key="send_button")
 
-    if send_button:
-        llm_response = "This is a response from LLM model"
+    if send_button or st.session_state.send_input:
+        if st.session_state.user_question != "":
+            llm_response = "This is a response from LLM model"
 
-        with chat_container:
-            st.chat_message("user").write(user_input)
-            st.chat_message("ai").write("here is your answer")
+            with chat_container:
+                st.chat_message("user").write(st.session_state.user_question)
+                st.chat_message("ai").write("here is your answer")
 
 if __name__ == "__main__":
     main()
