837f3d8f51c773ea1bf027bce32bb32d61fcb494
Himanshu Goswami <himgos@gmail.com>
2024-01-20 11:36:27 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Multi-Modal-GPT',
+    version='0.0.0',
+    author='Himanshu Goswami',
+    author_email='himgos@gmail.com',
+    packages=find_packages(),
+    install_requires = []
+)
