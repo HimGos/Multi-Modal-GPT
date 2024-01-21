488497fc22191121587d1485d4db0239ed07243f
Himanshu Goswami <himgos@gmail.com>
2024-01-21 14:29:53 +0530

    Image handler

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from src.llm_chains import load_normal_chain
 from src.utils import save_chat_history_json, get_timestamp, load_chat_history_json
 from src.audio_handler import transcribe_audio
+from src.image_handler import handle_image
 from langchain.memory import StreamlitChatMessageHistory
 from streamlit_mic_recorder import mic_recorder
 import yaml
@@ -18,8 +19,9 @@ def load_chain(chat_history):
 
 
 def clear_input_field():
-    st.session_state.user_question = st.session_state.user_input
-    st.session_state.user_input = ""
+    if st.session_state.user_question == "":
+        st.session_state.user_question = st.session_state.user_input
+        st.session_state.user_input = ""
 
 
 def set_send_input():
@@ -63,31 +65,48 @@ def main():
         st.session_state.history = []
 
     chat_history = StreamlitChatMessageHistory(key="history")
-    llm_chain = load_chain(chat_history)
 
     user_input = st.text_input("Type your message here", key='user_input', on_change=set_send_input)
 
     voice_recording_column, send_button_column = st.columns(2)
+    chat_container = st.container()
     with voice_recording_column:
         voice_recording = mic_recorder(start_prompt="Start recording", stop_prompt="Stop recording", just_once=True)
     with send_button_column:
         send_button = st.button("Send", key="send_button", on_click=clear_input_field)
 
     uploaded_audio = st.sidebar.file_uploader("Upload an audio file", type=['wav', 'mp3', 'ogg'])
+    uploaded_image = st.sidebar.file_uploader("Upload an image file", type=['jpeg', 'png', 'jpg'])
 
     if uploaded_audio:
-        transcribed_audio = transcribe_audio(uploaded_audio)
+        transcribed_audio = transcribe_audio(uploaded_audio.getvalue())
+        print(transcribed_audio)
+        llm_chain = load_chain(chat_history)
         llm_chain.run("Summarize this text: " + transcribed_audio)
 
     if voice_recording:
-        transcribed_audio = transcribe_audio(voice_recording['bytes'])
+        transcribed_audio = transcribe_audio(voice_recording["bytes"])
         print(transcribed_audio)
+        llm_chain = load_chain(chat_history)
         llm_chain.run(transcribed_audio)
 
     if send_button or st.session_state.send_input:
+        if uploaded_image:
+            with st.spinner("Processing image..."):
+                user_message = "Describe this image in detail please."
+                if st.session_state.user_question != "":
+                    user_message = st.session_state.user_question
+                    st.session_state.user_question = ""
+                llm_answer = handle_image(uploaded_image.getvalue(), st.session_state.user_question)
+                chat_history.add_user_message(user_message)
+                chat_history.add_ai_message(llm_answer)
+
         if st.session_state.user_question != "":
+            llm_chain = load_chain(chat_history)
             llm_response = llm_chain.run(st.session_state.user_question)
             st.session_state.user_question = ""
+
+        st.session_state.send_input = False
 
     if chat_history.messages != []:
         with chat_container:
