c5873727501c1acc9b8687500a1b675d0302af78
Himanshu Goswami <himgos@gmail.com>
2024-01-21 11:33:37 +0530

    Chat History Implementation

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 from src.llm_chains import load_normal_chain
+from src.utils import save_chat_history_json, get_timestamp, load_chat_history_json
 from langchain.memory import StreamlitChatMessageHistory
+from streamlit_mic_recorder import mic_recorder
+import yaml
+import os
+
+
+# loading config file
+with open('config.yaml', 'r') as f:
+    config = yaml.safe_load(f)
 
 
 def load_chain(chat_history):
@@ -17,13 +26,40 @@ def set_send_input():
     clear_input_field()
 
 
+def save_chat_history():
+    if st.session_state.history != []:
+        if st.session_state.session_key == "new_session":
+            st.session_state.new_session_key = get_timestamp() + ".json"
+            save_chat_history_json(st.session_state.history,
+                                    config['chat_history_path'] + st.session_state.new_session_key)
+        else:
+            save_chat_history_json(st.session_state.history,
+                                   config['chat_history_path'] + st.session_state.session_key)
+
+
 def main():
     st.title("Multimodal GPT")
     chat_container = st.container()
+    st.sidebar.title("Chat Sessions")
+    chat_sessions = ["new_session"] + os.listdir(config["chat_history_path"])
 
     if "send_input" not in st.session_state:
+        st.session_state.session_key = "new_session"
         st.session_state.send_input = False
         st.session_state.user_question = ""
+        st.session_state.new_session_key = None
+        st.session_state.session_index_tracker = "new_session"
+    if st.session_state.session_key == "new_session" and st.session_state.new_session_key is not None:
+        st.session_state.session_index_tracker = st.session_state.new_session_key
+        st.session_state.new_session_key = None
+
+    index = chat_sessions.index(st.session_state.session_index_tracker)
+    st.sidebar.selectbox("Select a chat session", chat_sessions, key="session_key", index=index)
+
+    if st.session_state.session_key != "new_session":
+        st.session_state.history = load_chat_history_json(config['chat_history_path'] + st.session_state.session_key)
+    else:
+        st.session_state.history = []
 
     chat_history = StreamlitChatMessageHistory(key="history")
     llm_chain = load_chain(chat_history)
@@ -38,8 +74,15 @@ def main():
             with chat_container:
                 st.chat_message("user").write(st.session_state.user_question)
                 llm_response = llm_chain.run(st.session_state.user_question)
-                st.chat_message("ai").write(llm_response)
                 st.session_state.user_question = ""
+
+    if chat_history.messages != []:
+        with chat_container:
+            st.write("Chat History:")
+            for message in chat_history.messages:
+                st.chat_message(message.type).write(message.content)
+
+    save_chat_history()
 
 
 if __name__ == "__main__":
